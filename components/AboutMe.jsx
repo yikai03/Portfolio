6ca0e67c79d650db5e7f4d9656d5829a93fdca66
Puah Yi Kai <puahyikai@gmail.com>
2023-12-10 18:58:34 +0800

    second push

---
+++ components/AboutMe.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { useTransition, useState } from "react";
+import TabButton from "./TabButton";
+
+//1d
+const TAB_DATA = [
+    {
+        title: "Skills",
+        id: "skills",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>Python</li>
+                <li>Numpy</li>
+                <li>Panda</li>
+                <li>R</li>
+                <li>Java</li>
+                <li>C++</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Experiences",
+        id: "experiences",
+        content: (
+          <ul className="list-disc pl-2">
+            <li>Tune Protect Hackathon</li>
+            <li>Hilti Ideathon</li>
+          </ul>
+        ),
+    },
+    {
+        title: "Education",
+        id: "education",
+        content: (
+          <ul className="list-disc pl-2">
+                <li>Asia Pacific University</li>
+                <li>Hin Hua High School</li>
+          </ul>
+        ),
+    }
+];
+//1
+const AboutMe = () => {
+    //1b
+    const[tab, setTab] = useState("skills");
+    const[isPending, startTransition] = useTransition();
+    const handleTabChange = (id) => {
+        startTransition(() => {
+            setTab(id);
+        });
+    };
+    //1a
+    return(
+        <section className="text-white grid" id="aboutMe">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:px-32 py-16 xl-gap-16 items-center">
+                <div>
+                    <img src="/AboutMe.png" alt="About Me" width={450} height={450} style={{ borderRadius: "25px" }}/>
+                </div>
+                <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+                    <h1 className="text-4xl font-bold text-white mb-4">About Me</h1>
+                    <p className="text-base text-justify lg:text-lg">I am currently pursuing a degree in Data Analytics at APU, where I have been honing my skills in various technologies. I am familiar in Python Programming, with an addition of Numpy and Panda library, which further enlarge my skillset.
+                        Additionally, I've created an interactable interface and dashboard through Google Looker Studio for data visualization platform.
+                        My passion and curiosity for technology and problem-solving has led me to participate few hackathon this year, and continuously expand my skillset within data analytic field and currently diving into frontend developer. </p>
+                    <div className="flex flex-row mt-8">
+{/* 1e */}
+                        <TabButton 
+                        selectTab={() => handleTabChange("skills")}
+                        active= {tab === "skills"}
+                        >
+                            
+                            Skills
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("experiences")}
+                        active= {tab === "experiences"}
+                        >
+                            {" "}
+                            Experiences{" "}
+                        </TabButton>
+
+                        <TabButton 
+                        selectTab={() => handleTabChange("education")}
+                        active= {tab === "education"}
+                        >
+                            {" "}
+                            Education{" "}
+                        </TabButton>
+                        
+                    </div>
+                    <div className="mt-8">{TAB_DATA.find((t) => t.id === tab).content}</div>
+                </div>
+
+            </div>
+        </section>
+
+    );
+};
+export default AboutMe;
