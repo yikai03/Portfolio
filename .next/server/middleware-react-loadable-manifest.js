0d799bd0be72e55202ccacbea140c800bbacafac
Puah Yi Kai <puahyikai@gmail.com>
2023-12-10 21:36:14 +0800

    9.36pm 41push

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"index.js -> ../components/HeroSection\":{\"id\":\"index.js -> ../components/HeroSection\",\"files\":[\"static/chunks/components_HeroSection_jsx.js\"]}}"
